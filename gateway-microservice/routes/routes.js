c6a5667ab17d17b195e587b1ae06019dfc275a97
jsilva <joao.m.silva@gsiportugal.com>
2023-11-14 19:31:02 +0000

    Start of gateway microservice

---
+++ gateway-microservice/routes/routes.js
@@ -0,0 +1,28 @@
+const ROUTES = [
+    {
+        url: '/free',
+        auth: false,
+        creditCheck: false,
+        proxy: {
+            target: "https://www.google.com",
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/free`]: '',
+            },
+        }
+    },
+    {
+        url: '/premium',
+        auth: true,
+        creditCheck: true,
+        proxy: {
+            target: "https://www.google.com",
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/premium`]: '',
+            },
+        }
+    }
+]
+
+exports.ROUTES = ROUTES;
