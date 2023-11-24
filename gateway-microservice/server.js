06fb6a24b2e1efc2618c685e51e383b5b47c4ada
jsilva <joao.m.silva@gsiportugal.com>
2023-11-24 21:13:37 +0000

    Add routes, rateLimi

--- gateway-microservice/server.js
+++ gateway-microservice/server.js
@@ -4,11 +4,15 @@ const {ROUTES} = require("./routes/routes");
 
 const {setupLogging} = require("./logging/logging");
 const {setupProxies} = require("./proxy/proxy");
+const {setupRateLimit} = require("./rateLimit/ratelimit");
+//const {setupAuth} = require("./auth/auth");
 
 const app = express();
 const port = 3002;
 
 setupLogging(app);
+//setupAuth(app, ROUTES);
+setupRateLimit(app, ROUTES);
 setupProxies(app, ROUTES);
 
 app.listen(port, () => {
