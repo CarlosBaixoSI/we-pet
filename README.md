9f69475e95a1f28e40b920997488a399f519a181
CarlosBeiramar <carlosbeiramar@gmail.com>
2023-12-01 18:22:42 +0000

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # we-pet
+
+## Install test dependencies:
+- npm install --save-dev jest
+- run tests: npx jest
+
+## Microsservices ports:
+- **Gateway** -> 3000
+- **Auth-microservice** -> 3001
+- **User-microsservice** -> 3002
+- **Animal-microsservice** -> 3003
+- **ad-management-microsservice** -> 3004
--- ad-management-microservice/app.js
+++ ad-management-microservice/app.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const app = express();
 
-//middleware
-app.use(express.json());
-
-const advertisementRouter = require("./routes/advertisementRoutes");
-
+// Load environment variables from .env file
 require('dotenv').config();
+
+// Connect to MongoDB using the mongoose service
 require("./services/mongoose/mongooseService");
 
-//start listening on port AUTH_PORT (defined in .env file)
+// Parse JSON bodies
+app.use(express.json());
+
+// Import advertisement routes
+const advertisementRouter = require("./routes/advertisementRoutes");
+
+// Start listening on the specified port (AD_MGMT_PORT from .env file or 3004)
 const port = process.env.AD_MGMT_PORT || 3004;
 
+// Use the advertisement routes for requests starting with /advertisements
 app.use("/advertisements", advertisementRouter);
 
+// Start the server
+// port should be 3004
 app.listen(port, () => {
   console.log("Server is running on port %s", port);
 });
 
+// Export the app module
 module.exports = app;
