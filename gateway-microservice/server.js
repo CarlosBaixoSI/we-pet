b4f65ac1cf23d3cbd5d5d88f7fab0061654bda14
João Silva <lesijoaosilva@gmail.com>
2024-04-09 17:08:07 +0100

    Update server.js

--- gateway-microservice/server.js
+++ gateway-microservice/server.js
@@ -12,7 +12,7 @@ const app = express();
 //app.use('/', routes);
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // setupLogging(app);
 setupProxies(app, ROUTES);
